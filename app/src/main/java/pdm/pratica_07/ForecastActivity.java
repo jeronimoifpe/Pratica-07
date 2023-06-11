0613270eebc9d685ef8c18a9922de184f703f3b4
jeronimo <jcm1@discente.ifpe.edu.br>
2023-06-11 01:26:32 -0300

    Parte 03 - Passos 03 e 04.

--- app/src/main/java/pdm/pratica_07/ForecastActivity.java
+++ app/src/main/java/pdm/pratica_07/ForecastActivity.java
@@ -2,7 +2,12 @@ package pdm.pratica_07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ForecastActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class ForecastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        LinearLayout layout = findViewById(R.id.layout_forecast);
+        Intent intent = getIntent();
+        ArrayList<CharSequence> data = intent.getCharSequenceArrayListExtra("data");
+        for (CharSequence entry : data) {
+            TextView text = new TextView(this);
+            text.setText(entry);
+            text.setTextSize(20);
+            text.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout.addView(text);
+        }
     }
 }
